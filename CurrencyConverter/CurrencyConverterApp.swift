da2e12e21125994b3c0c7ab2c2c29947622efc48
Diana Nuansengsy <diana.nuansengsi@mail.ru>
2022-02-07 22:41:23 +0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Диана Нуансенгси on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
